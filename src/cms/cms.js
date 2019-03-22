76a2f87c1d56d6805ca8d7ad7f3b25722a33d3b0
seanclendening <seanclendening@gmail.com>
2019-03-22 14:42:45 -0400

    Buid work

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,9 +3,7 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import HomePagePreview from './preview-templates/HomePagePreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('home', HomePagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
--- src/pages/privacy/index.js
+++ src/pages/privacy/index.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 
-const PrivacyPage = ({ props }) => {
-  return (
-    <Layout>
-      <div>Test Complete</div>
-    </Layout>
-  )
+// const Navbar = class extends React.Component {
+const PrivacyPage = class extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Layout>
+        <section className="section is-light">
+          <div className="container">
+            <h1 className="has-text-centered">We take your privacy seriously</h1>
+            <div className="box m-t-50">
+              <div className="tabs is-centered">
+                <ul>
+                  <li className="is-active is-size-3"><a>Privacy Policy</a></li>
+                  <li className="is-size-3"><a>Cookie Policy</a></li>
+                </ul>
+              </div>
+              <div>
+                <p><strong>Last Updated: January 01, 1967</strong></p>
+              </div>
+              <div className="content">
+                Insert content here.
+              </div>
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
+  }
 }
 
 export default PrivacyPage
