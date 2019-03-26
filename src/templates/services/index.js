7e245e52ebf37be7c05bde643d3065e61648de20
seanclendening <seanclendening@gmail.com>
2019-03-26 11:14:05 -0400

    Add body field

--- src/templates/services/index.js
+++ src/templates/services/index.js
@@ -1,26 +1,44 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import Content, { HTMLContent } from '../../components/Content'
 
-export const ServicesPageTemplate = ({ contentComponent }) => {
+export const ServicesPageTemplate = ({ hero, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
     <section className="section is-light">
       <div className="container">
-        <h1 className="has-text-centered">Services</h1>
+        <h1 className="has-text-centered">{hero.heading}</h1>
+        <h3 className="has-text-centered">{hero.subheading}</h3>
       </div>
     </section>
   )
 }
 
 const ServicesPage = ({ data }) => {
-  // const { markdownRemark: post } = data
+  const { markdownRemark: query } = data
   return(
     <Layout>
-      <ServicesPageTemplate contentComponent={HTMLContent} />
+      <ServicesPageTemplate
+        hero={query.frontmatter.hero}
+        contentComponent={HTMLContent} />
     </Layout>
   )
 }
 
 export default ServicesPage
+
+export const servicesPageQuery = graphql`
+  query ServicesPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        hero {
+          heading
+          subheading
+        }
+        body
+      }
+    }
+  }
+`
